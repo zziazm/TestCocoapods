6f2ed3c1ae53cb1aa5ec065db775c96ed04608bd
zziazm <1310726454@qq.com>
2018-04-16 14:17:14 +0800

    update

---
+++ UIViewCategory/UIView+ZMAdd.h
@@ -0,0 +1,13 @@
+//
+//  UIView+ZMAdd.h
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2018/4/16.
+//  Copyright © 2018年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ZMAdd)
+- (CGFloat)zm_left;
+@end
---
+++ UIViewCategory/UIView+ZMAdd.m
@@ -0,0 +1,15 @@
+//
+//  UIView+ZMAdd.m
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2018/4/16.
+//  Copyright © 2018年 zm. All rights reserved.
+//
+
+#import "UIView+ZMAdd.h"
+
+@implementation UIView (ZMAdd)
+- (CGFloat)zm_left{
+    return self.frame.origin.x;
+}
+@end
